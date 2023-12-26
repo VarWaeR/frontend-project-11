3fc44d048020054d9370e441d28700aa6106bbd3
Kirill <kirill.kltvkin@yandex.ru>
2023-12-26 17:03:55 +0300

    added parser

---
+++ src/parser.js
@@ -0,0 +1,28 @@
+export default (data) => {
+  const parser = new DOMParser();
+  const parsedData = parser.parseFromString(data, 'application/xml');
+
+  const error = parsedData.querySelector('parsererror');
+  if (error) {
+    const customError = new Error('An error occurred while parsing the XML data !');
+    customError.code = 'parseError';
+    throw customError;
+  }
+
+  const channel = parsedData.querySelector('channel');
+  const feedTitle = channel.querySelector('title').textContent;
+  const feedDescription = channel.querySelector('description').textContent;
+  const feed = { title: feedTitle, description: feedDescription };
+
+  const items = Array.from(parsedData.querySelectorAll('item'));
+
+  const posts = items.map((item) => {
+    const title = item.querySelector('title').textContent;
+    const description = item.querySelector('description').textContent;
+    const link = item.querySelector('link').textContent;
+
+    return { title, description, link };
+  });
+
+  return { feed, posts };
+};
